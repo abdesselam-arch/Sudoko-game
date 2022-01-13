bd937ee1a693552d1c5b213ddc92d4aa900b513a
Ahmed abdesselam <73743480+abdesselam-arch@users.noreply.github.com>
2022-01-13 17:17:20 +0100

    Add files via upload

---
+++ soduko att1.py
@@ -0,0 +1,37 @@
+import random
+
+def MakeSoduko():
+    Grid = [9][9]
+
+    for i in range (9):
+        for j in range (9):
+            Grid[i][j] = 0
+    printgrid(Grid)
+
+def Printgrid(Grid):
+    TableTD = "|---------------------------|"
+    tableMD = "|-----#------------#--------|"
+
+for line in board: print(line)
+
+Grid  = [
+    [6, 2, 5, 8, 4, 3, 7, 9, 1],
+    [7, 9, 1, 2, 6, 5, 4, 8, 3],
+    [4, 8, 3, 9, 7, 1, 6, 2, 5],
+    [8, 1, 4, 5, 9, 7, 2, 3, 6],
+    [2, 3, 6, 1, 8, 4, 9, 5, 7],
+    [9, 5, 7, 3, 2, 6, 8, 1, 4],
+    [5, 6, 9, 4, 3, 2, 1, 7, 8],
+    [3, 4, 2, 7, 1, 8, 5, 6, 9],
+    [1, 7, 8, 6, 5, 9, 3, 4, 2],
+]
+
+
+
+    for x in range(9):
+        for y in range(9):
+
+
+
+
+MakeSoduko()
